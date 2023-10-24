6bb2f2bd599ece4e39c3b98fa5c571a48be77dfe
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-24 11:33:39 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Provider } from "react-redux";
+import store from "../redux/store";
+import TagManager from "react-gtm-module";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    useEffect(() => {
+    const tagManagerArgs = {
+      gtmId: "GTM-WP9R782",
+    };
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+  return(
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
