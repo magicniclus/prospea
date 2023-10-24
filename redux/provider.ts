fe1e0136f7d774457a9adb0601af4fb81db215aa
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-24 11:33:43 +0200

    Create provider.ts

---
+++ redux/provider.ts
@@ -0,0 +1,13 @@
+// import store from "./store";
+// import { Provider as ReduxProviderComponent } from "react-redux";
+// import React, { ReactNode } from "react";
+
+// interface ReduxProviderProps {
+//   children: ReactNode;
+// }
+
+// const ReduxProvider: React.FC<ReduxProviderProps> = ({ children }) => {
+//   return <ReduxProviderComponent store={store}>{children}</ReduxProviderComponent>;
+// }
+
+// export default ReduxProvider;
